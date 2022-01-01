3112998a3b610ead3260e1580e53e4695e98074a
Femi Obadimu <ficazzosam@gmail.com>
2022-01-01 04:10:08 +0100

    REady for rust

---
+++ target/debug/deps/demo.d
@@ -0,0 +1,5 @@
+C:\Users\Femi\Desktop\Repositories\Rust\demo\target\debug\deps\demo.exe: src\main.rs
+
+C:\Users\Femi\Desktop\Repositories\Rust\demo\target\debug\deps\demo.d: src\main.rs
+
+src\main.rs:
