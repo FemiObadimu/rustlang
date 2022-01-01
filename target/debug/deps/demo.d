38378a8772a72b12dc87e870485740c6149903de
Femi Obadimu <ficazzosam@gmail.com>
2022-01-01 04:13:49 +0100

    Fully Ready

--- target/debug/deps/demo.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Femi\Desktop\Repositories\Rust\demo\target\debug\deps\demo.exe: src\main.rs
-
-C:\Users\Femi\Desktop\Repositories\Rust\demo\target\debug\deps\demo.d: src\main.rs
-
-src\main.rs:
